b172aa3325edcc3f9ca41808cf51d1b22b740ef9
Unknown Name <tommy6styx@gmail.com>
2016-08-16 21:28:12 -0700

    Create RasVNCInstall.sh
    Ras VNC Install

---
+++ RasVNCInstall.sh
@@ -0,0 +1,6 @@
+sudo apt-get install tightvncserver
+echo tightvncserver installation complete
+echo --------------------------------------------------
+echo --------------------------------------------------
+echo --------------------------------------------------
+echo Thank you for using mojtabaielec's software
